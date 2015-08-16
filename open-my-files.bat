9bd35ecc1606782130a80d0cd3a8731750206e79
aaronistheman <aarons.7007@gmail.com>
2015-08-16 12:20:06 -0700

    Slightly made the batch file more organized

--- open-my-files.bat
+++ open-my-files.bat
@@ -1,7 +1,7 @@
+start Notepad++ open-my-files.bat
+start Notepad++ todo-list.txt
 start Notepad++ index.html
 start Notepad++ play.html
 start Notepad++ css/bs.css
 start Notepad++ js/bs.js
-start Notepad++ todo-list.txt
-start Notepad++ open-my-files.bat
 exit
