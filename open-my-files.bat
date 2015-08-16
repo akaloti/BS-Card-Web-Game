bf36e0bf8dac9dd32a00bef6f4fd1b6b804d974c
aaronistheman <aarons.7007@gmail.com>
2015-08-16 12:45:21 -0700

    Removed some no longer needed parts of open-my-files.bat

--- open-my-files.bat
+++ open-my-files.bat
@@ -1,9 +1,7 @@
-start Notepad++ open-my-files.bat
 start Notepad++ todo-list.txt
 start Notepad++ index.html
 start Notepad++ play.html
 start Notepad++ css/bs.css
 start Notepad++ js/bs.js
 start Notepad++ settings.html
-start Notepad++ update-todo.sh
 exit
