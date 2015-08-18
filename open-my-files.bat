315221001035eb622d29eef7797dfca6c4d3b8aa
aaronistheman <aarons.7007@gmail.com>
2015-08-18 14:25:44 -0700

    Renamed file: js/bs.js -> js/index.js

--- open-my-files.bat
+++ open-my-files.bat
@@ -2,7 +2,7 @@ start Notepad++ todo-list.txt
 start Notepad++ index.php
 start Notepad++ play.php
 start Notepad++ css/bs.css
-start Notepad++ js/bs.js
+start Notepad++ js/index.js
 start Notepad++ settings.html
 start Notepad++ unit-tests.html
 start Notepad++ js/unit-tests.js
