188a8312145230bd32a87bc1babafd6ee2ad5eb6
aaronistheman <aarons.7007@gmail.com>
2015-08-18 21:06:44 -0700

    Separated the suit from the rank for instances of Card
    That is, suit and rank have their own member variables (rather than a
    shared one).

--- js/play.js
+++ js/play.js
@@ -3,8 +3,9 @@ bs.deck = [];
 bs.DECK_LENGTH = 52;
 bs.players = [];
 
-function Card(name) {
-  this.name = name;
+function Card(suit, rank) {
+  this.suit = suit;
+  this.rank = rank;
 
   // coordinates of image on sprite sheet
   // this.x = [get from list]
@@ -19,9 +20,7 @@ function Player() {
 
 // Precondition: none
 // Postcondition: bs.deck contains 52 unique instances of class Card,
-// each representing a unique card. Each instance's name consists of
-// two letters, the former
-// indicating the suit, and the latter indicating the rank.
+// each representing a unique card.
 // Note that bs.deck will be overritten.
 function generateDeck() {
   // Empty the deck
@@ -31,20 +30,11 @@ function generateDeck() {
   var ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J',
     'Q', 'K'];
 
-  var cardName = "";
   for (var s in suits) {
-    cardName += suits[s];
     for (var r in ranks) {
-      // append the rank
-      cardName += ranks[r];
-
       // console.log(cardName);
-      bs.deck.push(new Card(cardName));
-
-      // reset the card back to just the suit
-      cardName = suits[s];
+      bs.deck.push(new Card(suits[s], ranks[r]));
     }
-    cardName = "";
   }
 }
 
