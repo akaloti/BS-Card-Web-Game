b4e42847f9e22ed89b709e8e268e56b783c2a167
aaronistheman <aarons.7007@gmail.com>
2015-08-23 19:59:54 -0700

    User can unselect cards
    I've decided not to give the user a maximum number of cards that he can
    select. After all, in this game (BS), a player can submit more than four
    cards on his turn; whether he gets away with it or not is up to the other
    players.

--- js/play.js
+++ js/play.js
@@ -405,15 +405,38 @@ function setUpGame() {
     var keyCodeUp = 38;
     var keyCodeSpace = 32;
     if (e.which === keyCodeDown)
-      bs.currentHoveredCardIndex = updateHoveredCard(
         bs.currentHoveredCardIndex, "down");
+      bs.currentHoveredCardIndex = updateHoveredCard(
     else if (e.which === keyCodeUp)
       bs.currentHoveredCardIndex = updateHoveredCard(
         bs.currentHoveredCardIndex, "up");
     else if (e.which === keyCodeSpace)
-      $("#displayed-cards li:nth-child(" + (
-        bs.currentHoveredCardIndex + 1) + ')').addClass("picked");
+      selectOrUnselectCard();
   });
+}
+
+/*
+  @pre bs.currentHoveredCardIndex is updatd
+  @post if user can select the card marked by
+  bs.currentHoveredCardIndex, the card will be selected; if the card
+  was already selected, it will be unselected
+  @hasTest not yet
+  @returns nothing
+  @throws nothing
+*/
+function selectOrUnselectCard() {
+  var cardToAffect = "#displayed-cards li:nth-child(" + (
+    bs.currentHoveredCardIndex + 1) + ')';
+  var cssClassPicked = "picked";
+
+  if ($(cardToAffect).hasClass(cssClassPicked)) {
+    // deselect if card already is picked
+    $(cardToAffect).removeClass(cssClassPicked);
+  }
+  else {
+    // select an unselected card
+    $(cardToAffect).addClass(cssClassPicked);
+  }
 }
 
 $(document).ready(function(){
