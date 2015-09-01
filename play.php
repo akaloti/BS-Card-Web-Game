be08298e604ddab7b9ffdf7dc9b457b1825faa68
aaronistheman <aarons.7007@gmail.com>
2015-09-01 16:01:55 -0700

    Made a third style that looks sufficiently good and uses bg.jpg
    I appropriately updated the readme to state where the images came from.

--- play.php
+++ play.php
@@ -7,12 +7,11 @@
 
 <html>
 <head>
+<title>Play: BS</title>
 <script
   src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js">
 </script>
 <link rel="stylesheet" type="text/css" href="<?php echo $_POST['style']; ?>">
-
-<title>Play: BS</title>
 
 </head>
 <body>
